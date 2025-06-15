2ee53b82bccb213f2f5ba1b40735553f5b7e7c7e
AdityaHeree <be.extrause@gmail.com>
2025-06-15 20:54:04 +0530

    done

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -1,17 +1,71 @@
 package dev;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.aditya.Presentation;
 import dev.aditya.PresentationTools;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private  static final Logger log = LoggerFactory.getLogger(Main.class);
-    private static PresentationTools tools = new PresentationTools();
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final PresentationTools presentationTools = new PresentationTools();
 
     public static void main(String[] args) {
 
+        // Stdio Server Transport (Support for SSE also available)
+        var transportProvider = new StdioServerTransportProvider(new ObjectMapper());
+        // Sync tool specification
+        var syncToolSpecification = getSyncToolSpecification();
+
+        // Create a server with custom configuration
+        McpSyncServer syncServer = McpServer.sync(transportProvider)
+                .serverInfo("java-mcp-server", "0.0.1")
+                .capabilities(McpSchema.ServerCapabilities.builder()
+                        .tools(true)
+                        .logging()
+                        .build())
+                // Register tools, resources, and prompts
+                .tools(syncToolSpecification)
+                .build();
+
+        log.info("Starting Java MCP Server...");
+    }
+
+    private static McpServerFeatures.SyncToolSpecification getSyncToolSpecification() {
+        var schema = """
+            {
+              "type" : "object",
+              "id" : "urn:jsonschema:Operation",
+              "properties" : {
+                "operation" : {
+                  "type" : "string"
+                }
+              }
+            }
+            """;
+        var syncToolSpecification = new McpServerFeatures.SyncToolSpecification(
+                new McpSchema.Tool("get_presentations", "Get a list of all presentations from Java", schema),
+                (exchange, arguments) -> {
+                    // Tool implementation
+                    List<Presentation> presentations = presentationTools.getPresentations();
+                    List<McpSchema.Content> contents = new ArrayList<>();
+                    for (Presentation presentation : presentations) {
+                        contents.add(new McpSchema.TextContent(presentation.toString()));
+                    }
+                    return new McpSchema.CallToolResult(contents, false);
+                }
+        );
+        return syncToolSpecification;
     }
 }
